8e376a3f10b71b699b66de216c15926dbb60a254
AnnaAnnxNX <anna.anna.annanx@rambler.ru>
2022-07-17 16:18:03 +0300

    fix set amount ym

--- src/scripts/ymRenewAmountFunctions.js
+++ src/scripts/ymRenewAmountFunctions.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const ExcelJS = require('exceljs');
 const { fileInfoForReadFile, templates } = require('../consts.js');
-const { lutimes } = require('fs');
 
 const normalizeCells = (cells) => {
     return cells
@@ -47,9 +46,13 @@ const uniqueReadExcelFile = async (file, filenameForConstantsFile) => {
         console.log(nSkuColumn, nColumns);   
         // результат в двух вариантах - [col1, col2], {sku1: {все поля}}
         const columns = {};
-        columns['sku'] = info.foratters['sku'] ? worksheet.getColumn(nSkuColumn).values.slice(info.rowBeginProduct).map(info.foratters['sku']) : worksheet.getColumn(nSkuColumn).slice(info.rowBeginProduct);
+        columns['sku'] = info.foratters['sku']
+            ? worksheet.getColumn(nSkuColumn).values.slice(info.rowBeginProduct).map(info.foratters['sku'])
+            : worksheet.getColumn(nSkuColumn).values.slice(info.rowBeginProduct);
         info.columnsNames.forEach((header, i) => {
-            columns[header] = info.foratters[header] ? worksheet.getColumn(nColumns[i]).values.slice(info.rowBeginProduct).map(info.foratters[header]) : worksheet.getColumn(nColumns[i]).values.slice(info.rowBeginProduct);
+            columns[header] = info.foratters[header]
+                ? worksheet.getColumn(nColumns[i]).values.slice(info.rowBeginProduct).map(info.foratters[header])
+                : worksheet.getColumn(nColumns[i]).values.slice(info.rowBeginProduct);
         })
         return {columns, object: createObjectFromColumns(columns)};
     } catch (e) {
@@ -62,24 +65,17 @@ const writeYMAmount = async (content) => {
         let worksheet = null;
         const workbook = new ExcelJS.Workbook();
         await workbook.xlsx.readFile(path.resolve(__dirname, '..', 'inputFiles', templates.ymAmount));
-
-        // worksheet = workbook.worksheets[0];
         
         worksheet = await workbook.getWorksheet(fileInfoForReadFile['ymRenewSetAmount.xlsx'].tagName);
 
-        // const skuValues = content.columns.sku;
-        // skuValues.forEach((sku, i) => {
-        //     const row = worksheet.getRow(i + fileInfoForReadFile['ymRenewSetAmount.xlsx'].rowBeginProduct);
-        //     row.values = [, , 111, ,111];
-        //     row.commit();
-        // });
-
-        // worksheet.insertRow(5, [, , 111, ,111]);
-        // const a = worksheet.insertRows(5, [[111, 111],['a', 'b']]);
-        worksheet.getRow(5).values = [1,2,3];
-        // const a = worksheet.getColumn(3).values;
-        // console.log(a);
-        // worksheet.getColumn(5).values = ['1','2','3'];
+        const skuValues = content.columns.sku;
+        let rowForWrite = fileInfoForReadFile['ymRenewSetAmount.xlsx'].rowBeginProduct;
+        skuValues.forEach((sku, i) => {
+            if (sku && sku.toString().trim() && content.object[sku]['Остаток']) {
+                worksheet.getRow(rowForWrite).values =  ['', '', sku, '', content.object[sku]['Остаток']];
+                rowForWrite++;
+            }
+        });
         
         const filename = `./upload/result${Math.random().toString(36)}.xlsx`;
         await workbook.xlsx.writeFile(filename);
